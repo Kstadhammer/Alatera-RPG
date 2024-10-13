6e417be2f7e0939ee070c9079dc405e4c710d84c
Rubbeman <109215347+Kstadhammer@users.noreply.github.com>
2024-10-13 15:39:32 +0200

    up

--- js/weapons.js
+++ js/weapons.js
@@ -1,17 +1,17 @@
-export const monsters = [
-    { name: "slime", level: 2, health: 15, atk: 3, def: 1 },
-    { name: "fanged beast", level: 8, health: 60, atk: 12, def: 8 },
-    { name: "dragon", level: 20, health: 300, atk: 30, def: 25 },
-    { name: "wolf", level: 5, health: 20, atk: 7, def: 4 },
-    { name: "bear", level: 6, health: 25, atk: 10, def: 6 },
-    { name: "troll", level: 8, health: 35, atk: 14, def: 10 },
-    { name: "goblin", level: 3, health: 25, atk: 5, def: 3 },
-    { name: "orc", level: 7, health: 50, atk: 11, def: 7 },
-    { name: "giant spider", level: 6, health: 40, atk: 9, def: 5 },
-    { name: "dark elf", level: 10, health: 75, atk: 15, def: 12 },
-    { name: "minotaur", level: 12, health: 100, atk: 18, def: 14 },
-    { name: "skeleton warrior", level: 4, health: 30, atk: 6, def: 4 },
-    { name: "ghost", level: 5, health: 35, atk: 8, def: 3 },
-    { name: "chimera", level: 15, health: 150, atk: 22, def: 18 },
-    { name: "basilisk", level: 14, health: 120, atk: 20, def: 16 }
+export const weapons = [
+    { name: 'stick', power: 5, atk: 1 },
+    { name: 'dagger', power: 30, atk: 4 },
+    { name: 'claw hammer', power: 50, atk: 6 },
+    { name: 'sword', power: 100, atk: 10 },
+    { name: 'axe', power: 100, atk: 12 },
+    { name: 'club', power: 75, atk: 8 },
+    { name: 'mace', power: 80, atk: 9 },
+    { name: 'spear', power: 90, atk: 11 },
+    { name: 'bow', power: 70, atk: 7 },
+    { name: 'crossbow', power: 85, atk: 9 },
+    { name: 'staff', power: 60, atk: 5 },
+    { name: 'warhammer', power: 110, atk: 13 },
+    { name: 'scimitar', power: 95, atk: 11 },
+    { name: 'halberd', power: 105, atk: 12 },
+    { name: 'katana', power: 100, atk: 11 }
   ];
